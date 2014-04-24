8fbed1c5dd6b87d7b106115f8b453397d5d7a99f
Justin Watt <justincwatt@gmail.com>
2014-04-23 23:26:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+pctsearch.py
+============
+A simple python script that returns the closest PCT mile marker waypoint to a
+supplied latitude and longitude. It depends on the 2014_PCT.kml file which is
+zipped within the [halfmiles_pct_tracks_waypoints.kmz](http://www.pctmap.net/google/)
+file.
+
+To run, just supply a latitude+longitude coordinate (e.g. recieved from a device
+like the SPOT Satellite Messenger) and you'll get back the mile marker waypoint
+(note: half-mile markers are written in the format `0156-5`), the latitude and
+longitude of the mile marker, and the distance between the supplied coordinate
+and the mile marker as calculated by the haversine equation (as the crow files).
+
+```
+./pct-search.py 33.59556,-116.57129
+0156 33.59643,-116.57026 0.1mi
+```
---
+++ pct-search.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+import xml.etree.ElementTree as ET
+import re, sys
+from math import radians, cos, sin, asin, sqrt
+
+if len(sys.argv) == 3:
+    current_latitude = float(sys.argv[1])
+    current_longitude = float(sys.argv[2])
+elif len(sys.argv) == 2:
+    current_latitude, current_longitude = [float(x) for x in sys.argv[1].split(',')]
+else:
+    exit('Requires command line arguments: <latitude> <longitude> or <latitude,longitude>')
+
+# from http://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+
+    # 3963mi is the radius of the Earth
+    mi = 3963 * c
+    return mi
+
+tree = ET.parse('2014_PCT.kml')
+root = tree.getroot()
+
+shortest_distance = None
+shortest_mile_marker = None
+shortest_longitude = None
+shortest_latitude = None
+
+for placemark in root.iter('{http://www.opengis.net/kml/2.2}Placemark'):
+    name = placemark.find('{http://www.opengis.net/kml/2.2}name')
+    # there are other placemarks, we only want the ones that look like mile markers, e.g. 0156, 2668-5
+    if name is not None and re.match('\d{4}(\-5)?', name.text) is not None:
+        mile_marker = name.text
+        coordinates = placemark.find('{http://www.opengis.net/kml/2.2}Point/{http://www.opengis.net/kml/2.2}coordinates')
+        if coordinates is not None:
+            # coordinates have the following format: "longitude,latitude,0", e.g. -119.558163,37.727157,0
+            longitude, latitude = [float(x) for x in coordinates.text.split(',')][:2]
+            distance = haversine(current_longitude, current_latitude, longitude, latitude)
+            if shortest_distance is None or distance < shortest_distance:
+                shortest_distance = distance
+                shortest_mile_marker = mile_marker
+                shortest_latitude = latitude
+                shortest_longitude = longitude
+
+print shortest_mile_marker, str(shortest_latitude) + ',' + str(shortest_longitude), str(round(shortest_distance,1)) + 'mi'
